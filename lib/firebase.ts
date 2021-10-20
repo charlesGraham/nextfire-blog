16b5dea4b65f85b74f198959d77f367df4d34e98
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 10:05:20 -0700

    adjust firebase packages and login to firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8QzrPjQh4l_2OnvDfAMMJaVFI70hOAkY",
+    authDomain: "nextfire-blog-34937.firebaseapp.com",
+    projectId: "nextfire-blog-34937",
+    storageBucket: "nextfire-blog-34937.appspot.com",
+    messagingSenderId: "757568075661",
+    appId: "1:757568075661:web:212e1378e5797fb059efc4"
+  };
+
+  //initialize firebase app
+  if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig)
+  }
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  export const storage = firebase.storage();
