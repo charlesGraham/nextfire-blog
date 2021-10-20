effb196e6b483d93aeb9d9b072467765df0adfe0
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 15:22:20 -0700

    Loader component

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Loader from '../components/Loader'
+
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/username',
-        query: {username: 'charles' },
-      }}>
-        <a>Charles Profile</a>
-      </Link>
+      <Loader show />
     </div>
   )
 }
