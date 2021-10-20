52b1c18e085d8ee8547213d15a03882ea47e52ca
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 16:01:07 -0700

    create navbar + styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Navbar() {
-    return 
+    const { user, username } = { };
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button>FEED</button>
+                    </Link>
+                </li>
+
+                {/* user is signed in AND has a username */}
+                {username && (
+                    <>
+                        <li className="push-left">
+                            <Link href="/admin">
+                                <button className="btn-blue">Write Posts</button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={`/${username}`}>
+                                <img src={user?.photoURL} alt="" />
+                            </Link>
+                        </li>
+                    </>
+                )}
+
+                {/* user is not signed in OR has not created a username */}
+                {username && (
+                    <>
+                    <li>
+                        <Link href="/enter">
+                            <button className="btn-blue">Log In</button>
+                        </Link>
+                    </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
