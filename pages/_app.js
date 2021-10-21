b813a83658f6fa5e406e3d2227081d5887d28d6b
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 21:08:27 -0700

    listen to real-time auth updates in firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
-import { Toaster } from 'react-hot-toast'
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
-import { UserContext } from '../lib/context'
+import { Toaster } from 'react-hot-toast';
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
+import { UserContext } from '../lib/context';
+import { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 
 
 function MyApp({ Component, pageProps }) {
+
+  //grab current user from firebase
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    //turn off real-time subscription
+    let unsubscribe;
+
+    if (user) {
+      const ref = firestore.collection('users').doc(user.uid);
+      unsubscribe = ref.onSnapshot((doc) => {
+        setUsername(doc.data()?.username);
+      });
+    }
+
+
+  }, [user])
 
   return (
     <UserContext.Provider value={{ user: {}, username: 'charles' }}>
