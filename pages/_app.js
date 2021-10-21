899faeeaabc8bf3a7b8327c5c5aaf85780a80c9a
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 21:22:44 -0700

    transferred logic built in _app to hooks js file in lib directory

--- pages/_app.js
+++ pages/_app.js
@@ -2,37 +2,17 @@ import { Toaster } from 'react-hot-toast';
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import { UserContext } from '../lib/context';
-import { useState, useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../lib/firebase';
+import { useUserData } from '../lib/hooks';
 
 
 
 
 function MyApp({ Component, pageProps }) {
 
-  //grab current user from firebase
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    //turn off real-time subscription
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection('users').doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username);
-      });
-    } else {
-      setUsername(null);
-    }
-
-    return unsubscribe;
-  }, [user])
+  const userData = useUserData();
 
   return (
-    <UserContext.Provider value={{ user, username: 'charles' }}>
+    <UserContext.Provider value={userData}>
       <>
         <Navbar />
         <Component {...pageProps} />
