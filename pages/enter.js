63827a0bbd725e36e054f0c0a7c8e2173cf072a4
Charles Graham <charles@Charless-MacBook-Pro.local>
2021-10-20 19:39:21 -0700

    fixed overflow issue

--- pages/enter.js
+++ pages/enter.js
@@ -1,9 +1,40 @@
 import styles from '../styles/Home.module.css'
+import { auth, GoogleAuthProvider } from '../lib/firebase'
+import Image from 'next/image'
 
-export default function EnterPage({ }) {
+export default function Enter(props) {
+
+    const user = null;
+    const username = null;
+
     return(
         <main>
-            <h1>Sign Up</h1>
+            {user ?
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
         </main>
     )
+}
+
+// sign in with Google
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    };
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <Image src="/google.png" alt="" width={50} height={50} /> Sign in with Google
+        </button>
+    );
+}
+
+function SignOutButton() {
+    return<button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm() {
+
 }
